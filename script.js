d7941f4de564bfb438937a10b82c9996302aae9c
Raymond Mei <Raymondmei.rm@gmail.com>
2020-11-04 17:45:36 -0500

    Create canvas

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
+var canvas = document.querySelector("#drawingCanvas");
 
-function loadFunc(){
-    const canvas = document.querySelector("#drawingCanvas");
-    const context = canvas.getContext("2d");
+function loadFunc() {
 
-    const canvasContainer = document.querySelector("#canvasContainer");
-    canvas.width = canvasContainer.width;
-    canvas.height = canvasContainer.height;
+    var context = canvas.getContext("2d");
+
+    resizeWindow();
+    
+    var isDrawing = false;
+
+    function startPos(e){
+        isDrawing = true;
+        context.beginPath();
+        draw(e);
+    }
+    function endPos(){
+        isDrawing = false;
+        context.endPath();
+    }
+
+    function mousePos(e){
+        var rect = canvas.getBoundingClientRect();
+        var scaleX = canvas.width / rect.width;
+        var scaleY = canvas.height / rect.height;
+        return {
+            x: (e.clientX - rect.left) * scaleX,
+            y: (e.clientY - rect.top) * scaleY
+        }
+    }
+
+    function draw(e){
+        if(!isDrawing) return;
+        
+        context.lineWidth = 8;
+        context.lineCap = "round";
+
+        var pos = mousePos(e);
+
+        context.lineTo(pos.x, pos.y);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(pos.x, pos.y);
+    }
+    
+    canvas.addEventListener("mousedown", startPos);
+    canvas.addEventListener("mouseup", endPos);
+    canvas.addEventListener("mousemove", draw);
+
+}
+
+function resizeWindow() {
+    canvas.width = window.innerWidth-50;
+    canvas.height = window.innerHeight-50;
 }
 
 
+
+
 window.addEventListener("load", loadFunc());
+window.addEventListener("resize", resizeWindow());
